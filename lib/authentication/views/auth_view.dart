9ad3db7030e22e3bc502772b9828faa81687cf4d
Ikram Hasan <ikramhasan.dev@gmail.com>
2021-06-03 06:25:12 +0600

    Initialized Repo

---
+++ lib/authentication/views/auth_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AuthView extends StatelessWidget {
+  const AuthView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
